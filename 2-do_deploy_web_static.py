cfbf525f31596a7626de7da7a7f8403c051ddbe4
Sadick <122355398+Sadickachuli@users.noreply.github.com>
2023-10-15 20:15:03 +0200

    Update 2-do_deploy_web_static.py

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -7,7 +7,7 @@ from datetime import datetime
 from fabric.api import *
 import os
 
-env.hosts = ['3.91.70.201', '54.163.214.196']
+env.host = ['3.91.70.201', '54.163.214.196']
 env.user = "ubuntu"
 
 
@@ -29,24 +29,19 @@ def do_pack():
 
 def do_deploy(archive_path):
     """
-    Distribute archive.
+        Distribute archive.
     """
     if os.path.exists(archive_path):
         archived_file = archive_path[9:]
         newest_version = "/data/web_static/releases/" + archived_file[:-4]
         archived_file = "/tmp/" + archived_file
-
         put(archive_path, "/tmp/")
         run("sudo mkdir -p {}".format(newest_version))
-        run("sudo tar -xzf {} -C {}/".format(archived_file, newest_version))
+        run("sudo tar -xzf {} -C {}/".format(archived_file,
+                                             newest_version))
         run("sudo rm {}".format(archived_file))
-
-        # Remove the existing files in the destination folder
-        run("sudo rm -rf {}/web_static/*".format(newest_version))
-
-        # Move the new files to the parent directory
-        run("sudo mv {}/web_static/* {}".format(newest_version, newest_version))
-
+        run("sudo mv {}/web_static/* {}".format(newest_version,
+                                                newest_version))
         run("sudo rm -rf {}/web_static".format(newest_version))
         run("sudo rm -rf /data/web_static/current")
         run("sudo ln -s {} /data/web_static/current".format(newest_version))
