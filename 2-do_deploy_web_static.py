36066e1793ccdd75eff9a1651624e22abd247fd2
Sadick <122355398+Sadickachuli@users.noreply.github.com>
2023-10-15 20:06:54 +0200

    Update 2-do_deploy_web_static.py

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -35,15 +35,16 @@ def do_deploy(archive_path):
         archived_file = archive_path[9:]
         newest_version = "/data/web_static/releases/" + archived_file[:-4]
         archived_file = "/tmp/" + archived_file
+
         put(archive_path, "/tmp/")
         run("sudo mkdir -p {}".format(newest_version))
         run("sudo tar -xzf {} -C {}/".format(archived_file, newest_version))
         run("sudo rm {}".format(archived_file))
 
-        # Move contents individually
-        with cd(newest_version):
-            run("sudo mv web_static/* .")
+        # Remove the existing files in the destination folder
+        run("sudo rm -rf {}/web_static/*".format(newest_version))
 
+        run("sudo mv {}/web_static/* {}/".format(newest_version, newest_version))
         run("sudo rm -rf {}/web_static".format(newest_version))
         run("sudo rm -rf /data/web_static/current")
         run("sudo ln -s {} /data/web_static/current".format(newest_version))
