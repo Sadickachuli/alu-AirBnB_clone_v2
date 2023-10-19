6dccbb8b484e274e87fa68d6f23b44d16ad98144
Sadick <122355398+Sadickachuli@users.noreply.github.com>
2023-10-20 00:32:37 +0200

    Update 6-number_odd_or_even.py

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -1,46 +1,39 @@
 #!/usr/bin/python3
-""" Script that starts a Flask web application """
+"""Script that starts a Flask web application"""
 
-from flask import Flask
-from flask import render_template
+from flask import Flask, render_template
+
 app = Flask(__name__)
 
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
-    """ Display HBNB """
+    """Display HBNB"""
     return 'HBNB'
 
 
 @app.route('/c/<text>', strict_slashes=False)
 def c_is_fun(text):
-    """Display C followed
-    along with the text variable
-    """
+    """Display C followed by the text variable"""
     return 'C {}'.format(text.replace('_', ' '))
 
 
 @app.route('/python', strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
 def python_is_cool(text='is cool'):
-    """Display Python
-    along with the text variable
-    """
+    """Display Python along with the text variable"""
     return 'Python {}'.format(text.replace('_', ' '))
 
 
 @app.route('/number/<int:n>', strict_slashes=False)
 def number(n):
-    """Display n is a number
-    only if n is an integer
-    """
+    """Display n is a number only if n is an integer"""
     return '{} is a number'.format(n)
 
 
 @app.route('/number_template/<int:n>', strict_slashes=False)
 def number_template(n):
-    """Display HTML page only if n is an integer
-    """
+    """Display HTML page only if n is an integer"""
     return render_template('5-number.html', n=n)
 
 
@@ -49,13 +42,13 @@ def number_odd_or_even(n):
     """Display HTML page with odd or even integer
     States whether <n> is even or odd
     """
-    #checks if n is an interger
+    # Check if n is an integer
     if isinstance(n, int):
-        #determine if n is even or odd
+        # Determine if n is even or odd
         even_or_odd = "even" if n % 2 == 0 else "odd"
-        #Render the template and pass the
-        #value of n and even_or _odd to the template
-    return render_template('6-number_odd_or_even.html', n=n, even_or_odd=even_or_odd)
+        # Render the template and pass the
+        # value of n and even_or_odd to the template
+        return render_template('6-number_odd_or_even.html', n=n, even_or_odd=even_or_odd)
 
 
 if __name__ == '__main__':
