fd25ae5b44176e2b9749524a4691037abf38146d
Sadick <122355398+Sadickachuli@users.noreply.github.com>
2023-10-21 13:02:12 +0200

    Update 3-python_route.py

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -1,34 +1,34 @@
 #!/usr/bin/python3
-""" Script that starts a Flask web application. """
+
+"""Script that starts a Flask web application"""
 from flask import Flask
+
 app = Flask(__name__)
 
 
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
-    """ Function that displays "Hello HBNB!" """
-    return 'Hello HBNB!'
+    """Comment"""
+    return "Hello HBNB!"
 
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
-    """ Function that displays "HBNB" """
-    return 'HBNB'
+    """Comment"""
+    return "HBNB"
 
 
 @app.route('/c/<text>', strict_slashes=False)
-def c_is_fun(text):
-    """ Function that displays "C" followed
-    by the value of the text variable """
-    return 'C {}'.format(text.replace('_', ' '))
+def text_route(text):
+    """Comment"""
+    return "C {}".format(text.replace("_", " "))
 
 
 @app.route('/python', strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
-def python_is_cool(text='is cool'):
-    """ Function that displays "Python" followed
-    by the value of the text variable """
-    return 'Python {}'.format(text.replace('_', ' '))
+def text_route_python(text="is cool"):
+    """Comment"""
+    return "Python {}".format(text.replace("_", " "))
 
 
 if __name__ == '__main__':
